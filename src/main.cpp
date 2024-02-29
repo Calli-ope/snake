a9fc42a287c95137c590489e86673677fe53b62e
Konrad <konradm.studium@gmail.com>
2024-02-29 17:52:36 +0100

    Init new project

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
+    auto window = sf::RenderWindow{ { 1920u, 1080u }, "Snake" };
     window.setFramerateLimit(144);
 
     while (window.isOpen())
